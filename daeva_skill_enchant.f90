b75362082be5a3fd7ee6ed775bc3a3efbc49f9ff
Crush1989 <79810639+Crush1989@users.noreply.github.com>
2021-02-28 17:15:57 -0800

    Add files via upload

--- daeva_skill_enchant.f90
+++ daeva_skill_enchant.f90
@@ -12,59 +12,38 @@
       integer :: enchant_no_essence,enchant_with_essence
       double precision :: get_rate
 
-
-
-      print*, "Enter the starting level of your skill and hit enter: "
-      read(*,*) start_level
-      print*, "Enter the level you will start using daeva essences. &
-            If you won't use any then enter 15:"
-      read(*,*) use_de_lvl
-      print*, "How many simulations do you want to do? I suggest &
-            doing between 1,000 and 1,000,000: "
-      read(*,*) num_sim
+      call get_user_input(start_level,use_de_lvl,num_sim)
 
       open(unit = 7, file = 'sim_results.txt',status='unknown')
       
       call random_seed()
 
+      books_needed(:) = 0
+      avg = 0.0
+      tot_de = 0
 
-        books_needed(:) = 0
-        avg = 0.0
-        tot_de = 0
-
-      
       do i = 1,num_sim
         level = start_level
         num_books_used = 0
         num_de_used = 0
         do while (level .lt. 15)
-        
           call random_number(rand_number)
-
-! Found these rates on the forum (from KR), not sure if they are correct.
+! Get the rate appropriate for the level. Found these rates on 
+! the forum (from KR), not sure if they are correct.
           rate = get_rate(level)
-
-! 10 is a safe level, never use a daeva essence here!       
-          if (level .eq. 10) then
-! use enchant_with_essence function but don't increment number 
-! of daeva essences used.
-             level = enchant_with_essence(level,rand_number,rate)
-          else if ( level .lt. use_de_lvl ) then
-             level = enchant_no_essence(level,rand_number,rate)
-          else if ( level .ge. use_de_lvl ) then
-             level = enchant_with_essence(level,rand_number,rate)
-             num_de_used = num_de_used + 1
-          endif
+! Enchant the skill and use daeva essence when the user wanted to.
+          call enchant(level,rand_number,rate,use_de_lvl,num_de_used)
           num_books_used = num_books_used + 1
         enddo
         books_needed(num_books_used) = books_needed(num_books_used) + 1
         tot_de = tot_de + num_de_used
       enddo
+! Calculate average daeva essences needed.
+      tot_de = tot_de / num_sim 
 
-      tot_de = tot_de / num_sim
       print*, 'average number of daeva essensce needed: ', tot_de
       do i = 1,500
-! Write to results file, the first columns is the number of books used
+! Write to the results file, the first columns is the number of books used
 ! to +15 a skill and the second column is the number of times this
 ! amount of books was needed.
             write(7,*) i,books_needed(i)
@@ -92,7 +71,7 @@
       else
               enchant_no_essence = ol - 1
       endif
-
+      return
       end function enchant_no_essence
 
 ! Function that enchants daeva skills with daeva essences
@@ -108,8 +87,7 @@
       else
               enchant_with_essence = ol 
       endif
-
-
+      return
       end function enchant_with_essence
 
 ! Function that gets the correct enchant rate for a given level
@@ -130,7 +108,45 @@
         else if ( l .ge. 10 .and. l .lt. 15) then
               get_rate = 0.6
         endif
-
-
+      return
       end function get_rate
 
+      subroutine enchant (l,rn,r,udel,ndeu)
+
+      implicit none
+
+      integer :: l,ndeu,udel
+      double precision :: rn,r
+      integer :: enchant_no_essence,enchant_with_essence 
+
+      ! 10 is a safe level, never use a daeva essence here!       
+          if (l .eq. 10) then
+! use enchant_with_essence function but don't increment number 
+! of daeva essences used.
+             l = enchant_with_essence(l,rn,r)
+          else if ( l .lt. udel ) then
+             l = enchant_no_essence(l,rn,r)
+          else if ( l .ge. udel ) then
+             l = enchant_with_essence(l,rn,r)
+             ndeu = ndeu + 1
+          endif
+      return
+      end
+
+      subroutine get_user_input (sl,udel,ns)
+
+      implicit none
+
+      integer :: sl,udel,ns
+
+      print*, "Enter the starting level of your skill and hit enter: "
+      read(*,*) sl
+      print*, "Enter the level you will start using daeva essences. &
+            If you won't use any then enter 15:"
+      read(*,*) udel
+      print*, "How many simulations do you want to do? I suggest &
+            doing between 1,000 and 1,000,000: "
+      read(*,*) ns
+      return
+      end
+ 
